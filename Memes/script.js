d5603d377190bebb1cf5637f256335554933fddb
Riu Kumari <kumariritu9210@gmail.com>
2025-01-06 18:49:03 +0530

    add files

---
+++ Memes/script.js
@@ -0,0 +1,20 @@
+// Meme page
+let img = document.querySelector(".meme img");
+async function randomMemeGenerator(){
+    let res = await fetch("https://meme-api.com/gimme");
+    console.log(res);
+    let json = await res.json();
+    console.log(json);
+    let url = json.url;
+    console.log(url);
+    img.src = url;
+}
+randomMemeGenerator();
+let timer = setInterval(randomMemeGenerator, 2500);
+
+img.addEventListener("mouseover", function(){
+    clearInterval(timer);
+})
+img.addEventListener("mouseleave", function(){
+    timer = setInterval(randomMemeGenerator, 2500);
+})
